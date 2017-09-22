97b303e60eba09d8dcdea182df7bae6c45592651
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-22 14:07:07 -0400

    Define instance method to return time cat eats in AM/PM

--- cat.rb
+++ cat.rb
@@ -6,4 +6,20 @@ class Cat
     @meal_time = meal_time
   end
 
+  def meal_time
+    @meal_time
+  end
+
+  def eats_at
+    if meal_time <= 11
+      "#{ meal_time } AM"
+    else
+      "#{ meal_time - 12} PM"
+    end
+  end
 end
+
+cat_1 = Cat.new("Winston", "plants", 5)
+cat_2 = Cat.new("Lil Bub", "salmon", 14)
+
+puts cat_2.eats_at
