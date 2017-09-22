fc21cef9bdf950239a9d432f247cff91bc14c974
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-22 15:00:42 -0400

    Add quota and deliver methods and print example output

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -18,4 +18,28 @@ class Paperboy
     @earnings
   end
 
+  def quota
+    50 + @experience / 2
+  end
+
+  def deliver(start_address, end_address)
+    papers = (end_address - start_address).abs
+
+    if papers > quota
+      @earnings += quota * 0.25
+      @earnings += (papers - quota) * 0.5
+    else
+      @earnings += papers * 0.25
+    end
+
+    @experience += papers
+  end
 end
+
+
+cece = Paperboy.new("Cece")
+
+puts "#{cece.name}'s experience: #{cece.experience}, earnings: #{cece.earnings}, quota: #{cece.quota}"
+
+cece.deliver(100, 150)
+puts "#{cece.name}'s experience: #{cece.experience}, earnings: #{cece.earnings}, quota: #{cece.quota}"
