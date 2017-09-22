8dc799a34bee3849c5c9f9242198c406c89b5ed3
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-22 14:14:17 -0400

    Fix eats_at method with correct var after if, add meow method

--- cat.rb
+++ cat.rb
@@ -6,20 +6,22 @@ class Cat
     @meal_time = meal_time
   end
 
-  def meal_time
-    @meal_time
-  end
-
   def eats_at
-    if meal_time <= 11
-      "#{ meal_time } AM"
+    if @meal_time <= 11
+      "#{ @meal_time } AM"
     else
-      "#{ meal_time - 12} PM"
+      "#{ @meal_time - 12} PM"
     end
   end
+
+  def meow
+    "My name is #{ @name } and I eat #{ @preferred_food } at #{ eats_at }"
+  end
+
 end
 
 cat_1 = Cat.new("Winston", "plants", 5)
 cat_2 = Cat.new("Lil Bub", "salmon", 14)
 
-puts cat_2.eats_at
+puts cat_1.meow
+puts cat_2.meow
